15d71af5c809d236b7ef7eeaeb26d7e790daf825
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-08-08 08:52:04 +0000

    Updating Russian translation

--- language/russian/main.php
+++ language/russian/main.php
@@ -4,5 +4,8 @@
 // Копирайт: xoops.ws
 
 define("_MD_CHAT_ACCESSONLYUSERS", "Доступ к чату имеют только зарегистрированные пользователи");
-
+define("_MD_CHAT_MESSAGE", "Сообщение:"); //Сообщение:
+define("_MD_CHAT_SEND", "Отправить"); //Отправить
+define("_MD_CHAT_CHAT", "Чат!"); //Чат!
+define("_MD_CHAT_LOADING", "Идёт загрузка..."); //Идёт загрузка...
 ?>
