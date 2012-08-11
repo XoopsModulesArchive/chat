6c407fb63e7c544e04ac40fadd039edd4206398a
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- chat/language/russian/admin.php
+++ chat/language/russian/admin.php
@@ -3,13 +3,13 @@
 // Автор: andrey3761
 // Копирайт: xoops.ws
 
-define("_AM_CHAT_FORM_PERM", "Права");
+define("_AM_CHAT_FORM_PERM","Права");
 
-define("_AM_CHAT_PERM_DSC", "Права чата");
+define("_AM_CHAT_PERM_DSC","Права чата");
 
-define("_AM_CHAT_PERM_1", "Права на просмотр чата");
-define("_AM_CHAT_PERM_2", "Права удаление сообщений");
+define("_AM_CHAT_PERM_1","Права на просмотр чата");
+define("_AM_CHAT_PERM_2","Права удаление сообщений");
 
-define("_AM_CHAT_BC_PERM", "Права");
+define("_AM_CHAT_BC_PERM","Права");
 
 ?>
--- chat/language/russian/main.php
+++ chat/language/russian/main.php
@@ -3,14 +3,14 @@
 // Автор: andrey3761
 // Копирайт: xoops.ws
 
-define("_MD_CHAT_ACCESSONLYUSERS", "Доступ к чату имеют только зарегистрированные пользователи");
-define("_MD_CHAT_MESSAGE", "Сообщение:");
-define("_MD_CHAT_SEND", "Отправить");
-define("_MD_CHAT_CHAT", "Чат!");
-define("_MD_CHAT_LOADING", "Идёт загрузка...");
+define("_MD_CHAT_ACCESSONLYUSERS","Доступ к чату имеют только зарегистрированные пользователи");
+define("_MD_CHAT_MESSAGE","Сообщение:");
+define("_MD_CHAT_SEND","Отправить");
+define("_MD_CHAT_CHAT","Чат!");
+define("_MD_CHAT_LOADING","Идёт загрузка...");
 
 // JS
-define("_MD_CHAT_CONFIRMDELETE", "Вы действительно хотите удалить это сообщение?");
-define("_MD_CHAT_DELETE", "Удалить?");//Удалить
+define("_MD_CHAT_CONFIRMDELETE","Вы действительно хотите удалить это сообщение?");
+define("_MD_CHAT_DELETE","Удалить?");//Удалить
 
 ?>
