870ddfcbb7e64c874d1646922ca6ebef9a5fea2c
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/chat/trunk/chat/language/english/admin.php
+++ XoopsModules/chat/trunk/chat/language/english/admin.php
@@ -3,13 +3,13 @@
 // Автор: andrey3761
 // Копирайт: xoops.ws
 
-define("_AM_CHAT_FORM_PERM", "Permissions");
+define("_AM_CHAT_FORM_PERM","Permissions");
 
-define("_AM_CHAT_PERM_DSC", "Chat Permissions");
+define("_AM_CHAT_PERM_DSC","Chat Permissions");
 
-define("_AM_CHAT_PERM_1", "Permissions to view the chat");
-define("_AM_CHAT_PERM_2", "Permissions to delete messages");
+define("_AM_CHAT_PERM_1","Permissions to view the chat");
+define("_AM_CHAT_PERM_2","Permissions to delete messages");
 
-define("_AM_CHAT_BC_PERM", "Permissions");
+define("_AM_CHAT_BC_PERM","Permissions");
 
 ?>
